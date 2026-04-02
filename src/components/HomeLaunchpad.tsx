120735fba2b1465a254d0198cc499bfcdfea6f61
StanGirard <stan@quivr.app>
2026-04-02 23:06:24 +0200

    Adjust hero vertical alignment and set dev port to 3678

--- src/components/HomeLaunchpad.tsx
+++ src/components/HomeLaunchpad.tsx
@@ -95,7 +95,7 @@ export function HomeLaunchpad() {
       {/* Scroll hint */}
       {!isOverlayOpen && (
         <motion.div
-          className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
+          className="absolute bottom-16 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={createTransition(0.6, 2)}
