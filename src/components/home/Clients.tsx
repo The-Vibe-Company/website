406f60cf0807c8f8456ad6097576d47e930fd72a
V-444 <victor@thevibecompany.co>
2026-07-02 12:30:43 +0200

    feat(home): add Trusk, LocService, Zapa + equal-size logo boxes

--- src/components/home/Clients.tsx
+++ src/components/home/Clients.tsx
@@ -9,6 +9,9 @@ const CLIENTS: Client[] = [
   { name: "Agence France-Presse", src: "/images/clients/afp.svg" },
   { name: "Coup de Pâtes", src: "/images/clients/coup-de-pates.svg" },
   { name: "Monka", src: "/images/clients/monka.webp" },
+  { name: "Trusk", src: "/images/clients/trusk.svg" },
+  { name: "LocService", src: "/images/clients/locservice.png" },
+  { name: "Zapa", src: "/images/clients/zapa.svg" },
 ];
 
 export function Clients() {
@@ -19,12 +22,15 @@ export function Clients() {
       </p>
       <Marquee>
         {CLIENTS.map((client) => (
-          <div key={client.name} className="mx-10 flex shrink-0 items-center md:mx-16">
+          <div
+            key={client.name}
+            className="mx-6 flex h-10 w-32 shrink-0 items-center justify-center md:mx-8 md:h-12 md:w-44"
+          >
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src={client.src}
               alt={client.name}
-              className="h-8 w-auto object-contain md:h-10"
+              className="max-h-full max-w-full object-contain"
               loading="eager"
               decoding="async"
             />
